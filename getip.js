3ff83ac85e495a2d111192a2cacf7ae28161e74c
lm379 <2645081603@qq.com>
2024-10-14 22:35:58 +0800

    修改IP查询接口

--- getip.js
+++ getip.js
@@ -1,10 +1,18 @@
-fetch('https://token.ip.api.useragentinfo.com/json?token=ab28a017dc0b7536f452fd951aed51d2')
+fetch('https://webapi-pc.meitu.com/common/ip_location')
     .then(response => response.json())
     .then(data => {
-        const ip = data.ip;
-        const province = data.province;
-        const city = data.city;
-        const welcomeMessage = `欢迎${province}${city}（IP：${ip}）的小伙伴访问本站`;
+        const ip = Object.keys(data.data)[0];
+        const locationData = data.data[ip];
+        const province = locationData.province;
+        const city = locationData.city;
+        const area_code = locationData.area_code;
+        const nation = locationData.nation;
+        let welcomeMessage;
+        if (area_code === '86' ) {
+            welcomeMessage = `欢迎${province}${city}（IP：${ip}）的小伙伴访问本站`;
+        } else {
+            welcomeMessage = `Welcome to visit our website from ${nation} (IP: ${ip})`;
+        }
         document.getElementById('welcome').textContent = welcomeMessage;
     })
     .catch(error => console.error(error));
